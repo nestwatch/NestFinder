46112546e2dffe1d62a5e5b3411a8051e7eb84f1
Shijin Jayaprakash <shijinjayaprakash@Shijins-MacBook-Pro.local>
2024-08-09 16:50:59 -0400

    First Deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    // ...other Next.js config settings...
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.module.rules.push({
+          test: /\.(js|jsx|ts|tsx)$/,
+          use: {
+            loader: 'next-babel-loader',
+            options: {
+              isServer,
+              hasReactRefresh: false,
+              // ...other options...
+            },
+          },
+        });
+      }
+  
+      return config;
+    },
+  };
+  
